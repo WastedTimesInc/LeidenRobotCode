6b82a1ae19f4ecc1210fa0fae10b0f68a5ca94a0
Hector van der Aa <h3cx@h3cx.dev>
2025-05-23 23:19:02 +0200

    motorctrl comment

--- motorctrl.cpp
+++ motorctrl.cpp
@@ -1,36 +1,36 @@
 #include "motorctrl.h"
 
 void writeMotor(uint8_t MOTOR, uint8_t DIR, uint8_t SPEED) {
-  switch (MOTOR) {
-    case 0:
-      switch (DIR) {
-        case 0:
-          CURRENT = (M_NULL & MOTOR_A_MASK) | (CURRENT & MOTOR_A_INV_MASK);
-          MOTOR_A_STATE = 0;
+  switch (MOTOR) { // Switch to select between motors
+    case 0: // Left motor selected
+      switch (DIR) { // Switch to select direction
+        case 0: // Motor stopped
+          CURRENT = (M_NULL & MOTOR_A_MASK) | (CURRENT & MOTOR_A_INV_MASK); // Append current to stop selected motor
+          MOTOR_A_STATE = 0; // Write motor state stopped
           break;
         case 1:
-          CURRENT = (MOTOR_A_FWD & MOTOR_A_MASK) | (CURRENT & MOTOR_A_INV_MASK);
-          MOTOR_A_STATE = 1;
+          CURRENT = (MOTOR_A_FWD & MOTOR_A_MASK) | (CURRENT & MOTOR_A_INV_MASK); // Append current to fwd selected motor
+          MOTOR_A_STATE = 1; // Write motor state fwd
           break;
         case 2:
-          CURRENT = (MOTOR_A_REV & MOTOR_A_MASK) | (CURRENT & MOTOR_A_INV_MASK);
-          MOTOR_A_STATE = 2;
+          CURRENT = (MOTOR_A_REV & MOTOR_A_MASK) | (CURRENT & MOTOR_A_INV_MASK); // Append current to rev selected motor
+          MOTOR_A_STATE = 2; // Write motor state rev
           break;
-        default:
+        default: // Error returns function
           Serial.println("OUT OF RANGE");
           return;
       }
-      writeI2C(CTRL_ADDR, CURRENT);
+      writeI2C(CTRL_ADDR, CURRENT); // Write appended CURRENT to motor control chip
 
-      if (SPEED < 256) {
-        analogWrite(SPD_PIN_A, SPEED);
-        MOTOR_A_SPEED = SPEED;
+      if (SPEED < 256) { // Validate speed (not really needed since uint8_t)
+        analogWrite(SPD_PIN_A, SPEED); // Write speed to corresponding motor pwm pin
+        MOTOR_A_SPEED = SPEED; // Update motor speed state
       } else {
-        Serial.println("OUT OF RANGE");
+        Serial.println("OUT OF RANGE"); // Out of range error (should NEVER occur)
       }
       break;
 
-    case 1:
+    case 1: // All comments same as above but for motor B (right)
       switch (DIR) {
         case 0:
           CURRENT = (M_NULL & MOTOR_B_MASK) | (CURRENT & MOTOR_B_INV_MASK);
@@ -58,14 +58,14 @@ void writeMotor(uint8_t MOTOR, uint8_t DIR, uint8_t SPEED) {
       }
       break;
 
-    default:
+    default: // Error handling for unknown motor
       Serial.println("INVALID MOTOR");
       break;
   }
 }
 
 void writeMotor(uint8_t DIR0, uint8_t SPEED0, uint8_t DIR1, uint8_t SPEED1) {
-  // Motor A control
+  // Motor A control, similar arch to above
   switch (DIR0) {
     case 0:
       CURRENT = (M_NULL & MOTOR_A_MASK) | (CURRENT & MOTOR_A_INV_MASK);
@@ -84,7 +84,7 @@ void writeMotor(uint8_t DIR0, uint8_t SPEED0, uint8_t DIR1, uint8_t SPEED1) {
       return;
   }
 
-  // Motor B control
+  // Motor B control, similar arch to above
   switch (DIR1) {
     case 0:
       CURRENT = (M_NULL & MOTOR_B_MASK) | (CURRENT & MOTOR_B_INV_MASK);
--- motorctrl.h
+++ motorctrl.h
@@ -4,7 +4,19 @@
 #include <Arduino.h>
 #include "commonutils.h"
 #include "globals.h"
-
+// writeMotor
+// This write a motor to a given speed and direction, available in two configs
+//
+// writeMotor(uint8_t MOTOR, uint8_t DIR, uint8_t SPEED)
+//    Write the speed and direction of just one motor
+//    MOTOR : 0=left, 1=right
+//    DIR   : 0=stopped, 1=fwd, 2=rev
+//    SPEED : 0-255
+//
+// writeMotor(uint8_t DIR0, uint8_t SPEED0, uint8_t DIR1, uint8_t SPEED1)
+//    Write the speed and direction for both motors
+//    DIR0(1)   : 0=stopped, 1=fwd, 2=rev
+//    SPEED0(1) : 0-255
 void writeMotor(uint8_t MOTOR, uint8_t DIR, uint8_t SPEED);
 void writeMotor(uint8_t DIR0, uint8_t SPEED0, uint8_t DIR1, uint8_t SPEED1);
 
